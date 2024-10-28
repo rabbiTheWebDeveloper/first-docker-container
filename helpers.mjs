467a9bfb8d1bd313bd10acff0d4a6224321fc5bd
S M AL RABBI <rabbithedeveloper@gmail.com>
2024-10-28 12:09:02 +0600

    docker useing code

--- helpers.mjs
+++ helpers.mjs
@@ -9,3 +9,7 @@ const connectToDatabase = () => {
 };
 
 export default connectToDatabase;
+
+
+// docker build .
+// docker run -p 5000:5000 sha256:16a4b6c68b1755bf755af3f9b2b6c0fa5faf8e8a9bf29ceafa5c2bbad5249876
